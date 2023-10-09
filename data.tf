94085fbf92186493ebbb12c36b677db193263680
Alex Bance <Alex.Bance@justice.gov.uk>
2023-10-09 11:02:28 +0100

    fix: update var definiton & logic to allow passing of vnet id

--- data.tf
+++ data.tf
@@ -1,6 +1,7 @@
 data "azurerm_virtual_network" "initiator" {
   provider = azurerm.initiator
 
+  count               = var.peerings["source"].vnet_id == null ? 1 : 0
   name                = var.peerings["source"]["vnet"]
   resource_group_name = var.peerings["source"]["resource_group"]
 }
@@ -8,6 +9,7 @@ data "azurerm_virtual_network" "initiator" {
 data "azurerm_virtual_network" "target" {
   provider = azurerm.target
 
+  count               = var.peerings["target"].vnet_id == null ? 1 : 0
   name                = var.peerings["target"]["vnet"]
   resource_group_name = var.peerings["target"]["resource_group"]
 }
--- inputs-required.tf
+++ inputs-required.tf
@@ -1,4 +1,18 @@
 variable "peerings" {
-  type        = map(any)
-  description = "Map of peerings to be created"
+  type = map(object({
+    name           = string
+    resource_group = optional(string)
+    vnet_name      = optional(string)
+    vnet_id        = optional(string)
+  }))
+  description = "Map of peerings to be created, must contain a source and target keys."
+  validation {
+    condition = alltrue([
+      contains(keys(var.peerings), "source"),
+      contains(keys(var.peerings), "target"),
+      (var.peerings["source"].vnet_name != null && var.peerings["source"].resource_group != null) || var.peerings["source"].vnet_id != null,
+      (var.peerings["target"].vnet_name != null && var.peerings["target"].resource_group != null) || var.peerings["target"].vnet_id != null,
+    ])
+    error_message = "Must contain exactly two keys, source and target and either vnet_name or vnet_id must be provided."
+  }
 }
