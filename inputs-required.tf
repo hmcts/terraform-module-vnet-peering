30f9014a4ca387bb558772dec91ddccda5ccf8bc
Alex Bance <Alex.Bance@justice.gov.uk>
2025-04-04 12:03:50 +0100

    fix: update condition to check valid options are passed

--- inputs-required.tf
+++ inputs-required.tf
@@ -1,15 +1,16 @@
 variable "peerings" {
   type = map(object({
     name           = string
-    resource_group = string
-    vnet           = string
+    resource_group = optional(string)
+    vnet           = optional(string)
     vnet_id        = optional(string)
   }))
   description = "Map of peerings to be created, must contain a source and target keys."
   validation {
     condition = alltrue([
       contains(keys(var.peerings), "source"),
-      contains(keys(var.peerings), "target")
+      contains(keys(var.peerings), "target"),
+      [for k, v in var.peerings : (contains(keys(v), "vnet") && contains(keys(v), "resource_group")) || contains(keys(v), "vnet_id")]
     ])
     error_message = "Must contain exactly two keys, source and target."
   }
